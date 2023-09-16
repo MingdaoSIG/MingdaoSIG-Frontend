52900d41a8d24820036a35e39f750f3d7f9c7c8a
banahaker <pn0818x@outlook.com>
2023-09-16 21:29:37 +0800

    feat: background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
       <head>
         <title>{title}</title>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="wrap">{children}</div>
+      </body>
     </html>
   );
 }
