ec8b4d0f89ba5bdd04c7f22768b97298b3844f6a
banahaker <pn0818x@outlook.com>
2023-09-17 21:47:11 +0800

    feat: split layout

---
+++ app/(Layout)/splitBlock.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./splitBlock.module.scss";
+
+const SplitBlock = ({ children }: { children: React.ReactNode }) => {
+  const blocks = React.Children.toArray(children);
+
+  return (
+    <>
+      <div className={style.app}>
+        <div className={style.mainview}>{blocks[0]}</div>
+        <div className={style.second}>{blocks[1]}</div>
+      </div>
+    </>
+  );
+};
+
+export default SplitBlock;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBar from "./(Layout)/HeaderBar";
+import SplitBlock from "./(Layout)/splitBlock";
 
 const Home = () => {
   return (
     <>
       <HeaderBar></HeaderBar>
+      <SplitBlock>
+        <div>123</div>
+        <div>234</div>
+      </SplitBlock>
     </>
   );
 };
