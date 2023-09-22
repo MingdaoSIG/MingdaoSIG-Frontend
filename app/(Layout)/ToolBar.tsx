2588911b28ea0c76af064d3ad901415cd8ea6ffb
HACO <jasonytonlinecomeandsee@gmail.com>
2023-09-22 11:35:10 +0800

    feat: ToolBar

--- app/(Layout)/ToolBar.tsx
+++ app/(Layout)/ToolBar.tsx
@@ -1,10 +1,12 @@
 const ToolBar = () => {
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="w-full absolute bottom-[64px]">
+      <div className="toolbar h-[90px] w-[484px] bg-white bg-opacity-50 mx-auto rounded-full">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
