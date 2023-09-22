787a67daf1744b44f6385434564c03d980915993
HACO <jasonytonlinecomeandsee@gmail.com>
2023-09-22 11:50:48 +0800

    Update ToolBar.tsx

--- app/(Layout)/ToolBar.tsx
+++ app/(Layout)/ToolBar.tsx
@@ -1,11 +1,47 @@
+import Image from "next/image";
+
 const ToolBar = () => {
   return (
     <div className="w-full absolute bottom-[64px]">
-      <div className="toolbar h-[90px] w-[484px] bg-white bg-opacity-50 mx-auto rounded-full">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="h-[90px] w-[484px] bg-white bg-opacity-50 mx-auto rounded-full border-white border-opacity-60 border">
+        <div className="h-full w-[350px] mx-auto flex">
+          <div className="flex-1 my-auto">
+            <Image
+              src={"/icons/bx-home-circle.svg"}
+              height={32}
+              width={32}
+              alt="bookmark"
+              className="cursor-pointer mx-auto"
+            ></Image>
+          </div>
+          <div className="flex-1 my-auto">
+            <Image
+              src={"/icons/bx-user.svg"}
+              height={32}
+              width={32}
+              alt="bookmark"
+              className="cursor-pointer mx-auto"
+            ></Image>
+          </div>
+          <div className="flex-1 my-auto">
+            <Image
+              src={"/icons/bxs-bell-ring.svg"}
+              height={32}
+              width={32}
+              alt="bookmark"
+              className="cursor-pointer mx-auto"
+            ></Image>
+          </div>
+          <div className="flex-1 my-auto">
+            <Image
+              src={"/icons/bx-menu-alt-right.svg"}
+              height={32}
+              width={32}
+              alt="bookmark"
+              className="cursor-pointer mx-auto"
+            ></Image>
+          </div>
+        </div>
       </div>
     </div>
   );
