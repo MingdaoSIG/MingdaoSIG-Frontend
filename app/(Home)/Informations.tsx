23b8bfec85abaa8296bfc5ab07afcf72cd3ab2c1
HACO <jasonytonlinecomeandsee@gmail.com>
2023-09-23 23:51:47 +0800

    feat: hashtag name

--- app/(Home)/Informations.tsx
+++ app/(Home)/Informations.tsx
@@ -1,10 +1,13 @@
 import style from "./Informations.module.scss";
 
-const Hashtag = () => {
+const Hashtag = (child: any) => {
   return (
+    // <div className="bg-[#00C2FF] rounded-[5px] py-1.5 px-1 grid-span-auto">
+    //   {child.name}
+    // </div>
     <div className="my-3">
       <h2 className="text-md-dark-green text-lg font-medium tracking-widest cursor-pointer hover:underline  hover:decoration-1 transition  duration-300 ease-out  ">
-        #AI
+        #{child.name}
       </h2>
       <p className="text-[#A6A6A6] text-xs font-extralight tracking-widest">
         23 post
@@ -28,9 +31,11 @@ const Information = () => {
         <h2 className=" text-black text-base font-extralight tracking-widest">
           Hashtags
         </h2>
-        <Hashtag></Hashtag>
-        <Hashtag></Hashtag>
-        <Hashtag></Hashtag>
+        <div className="">
+          <Hashtag name="資安" />
+          <Hashtag name="AI" />
+          <Hashtag name="法律" />
+        </div>
       </div>
       <div className={style.sigs}>
         <h2 className=" text-black mt-10 mb-3 text-base font-extralight tracking-widest">
