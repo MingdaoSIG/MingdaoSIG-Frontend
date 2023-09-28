7c97cc3bf00e529dad68c681283b5ab5d0d0ed5b
banahaker <pn0818x@outlook.com>
2023-09-28 16:03:02 +0800

    fix: toolbar scaling problem

--- app/(Layout)/ToolBar.tsx
+++ app/(Layout)/ToolBar.tsx
@@ -8,52 +8,50 @@ const menu = { home: "/", user: "/user", ring: "notify", menu: "/menu" };
 const ToolBar = () => {
   const pathname = usePathname();
   return (
-    <div className="w-full absolute bottom-[1em] select-none">
-      <div className="h-[5rem] w-[25rem] bg-white bg-opacity-50 mx-auto rounded-full border-white border-opacity-60 border">
-        <div className="h-full w-[350px] mx-auto flex">
-          <div
-            className={
-              "flex-1 my-auto bg-slate-400 rounded-full py-2 " +
-              (pathname.startsWith(menu["home"])
-                ? "bg-opacity-30"
-                : "bg-opacity-0")
-            }
-          >
-            <Image
-              src={"/icons/bx-home-circle.svg"}
-              height={32}
-              width={32}
-              alt="home"
-              className="cursor-pointer mx-auto"
-            ></Image>
-          </div>
-          <div className="flex-1 my-auto">
-            <Image
-              src={"/icons/bx-user.svg"}
-              height={32}
-              width={32}
-              alt="user"
-              className="cursor-pointer mx-auto"
-            ></Image>
-          </div>
-          <div className="flex-1 my-auto">
-            <Image
-              src={"/icons/bxs-bell-ring.svg"}
-              height={32}
-              width={32}
-              alt="ring"
-              className="cursor-pointer mx-auto"
-            ></Image>
-          </div>
-          <div className="flex-1 my-auto">
-            <Image
-              src={"/icons/bx-menu-alt-right.svg"}
-              height={32}
-              width={32}
-              alt="menu"
-              className="cursor-pointer mx-auto"
-            ></Image>
-          </div>
+    <div className="h-[5rem] w-[25rem] bg-white bg-opacity-50 mx-auto rounded-full border-white border-opacity-60 border">
+      <div className="h-full w-[22em] mx-auto flex">
+        <div
+          className={
+            "flex-1 my-auto bg-slate-400 rounded-full py-2 " +
+            (pathname.startsWith(menu["home"])
+              ? "bg-opacity-30"
+              : "bg-opacity-0")
+          }
+        >
+          <Image
+            src={"/icons/bx-home-circle.svg"}
+            height={32}
+            width={32}
+            alt="home"
+            className="cursor-pointer mx-auto"
+          ></Image>
+        </div>
+        <div className="flex-1 my-auto">
+          <Image
+            src={"/icons/bx-user.svg"}
+            height={32}
+            width={32}
+            alt="user"
+            className="cursor-pointer mx-auto"
+          ></Image>
+        </div>
+        <div className="flex-1 my-auto">
+          <Image
+            src={"/icons/bxs-bell-ring.svg"}
+            height={32}
+            width={32}
+            alt="ring"
+            className="cursor-pointer mx-auto"
+          ></Image>
+        </div>
+        <div className="flex-1 my-auto">
+          <Image
+            src={"/icons/bx-menu-alt-right.svg"}
+            height={32}
+            width={32}
+            alt="menu"
+            className="cursor-pointer mx-auto"
+          ></Image>
         </div>
       </div>
     </div>
