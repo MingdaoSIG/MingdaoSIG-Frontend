c36b434ba31721ff1956b6c8bf6b7e7a8ea20c28
HACO <jasonytonlinecomeandsee@gmail.com>
2023-10-03 01:02:37 +0800

    fix: toolbar

--- app/(Layout)/ToolBar.tsx
+++ app/(Layout)/ToolBar.tsx
@@ -4,29 +4,6 @@ import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const menu = [
-  {
-    name: "home",
-    path: "/",
-    icon: "/icons/bx-home-circle.svg",
-  },
-  {
-    name: "user",
-    path: "/@",
-    icon: "/icons/bx-user.svg",
-  },
-  {
-    name: "new",
-    path: "/new",
-    icon: "/icons/plus-circle.svg",
-  },
-  {
-    name: "hash",
-    path: "/#",
-    icon: "/icons/hash.svg",
-  },
-];
-
 const ToolBar = () => {
   const [userID, setUserID] = useState("/");
   const pathname = usePathname();
@@ -39,30 +16,82 @@ const ToolBar = () => {
     setUserID(_localStorage.customId);
   }, []);
 
+  const menu = [
+    {
+      name: "home",
+      path: "/",
+      route: "/",
+      icon: "/icons/bx-home-circle.svg",
+    },
+    {
+      name: "user",
+      path: "/@",
+      route: `/@${userID}`,
+      icon: "/icons/bx-user.svg",
+    },
+    {
+      name: "new",
+      path: "/new",
+      route: "/new",
+      icon: "/icons/plus-circle.svg",
+    },
+    {
+      name: "hash",
+      path: "/#",
+      route: "/#hashtag",
+      icon: "/icons/hash.svg",
+    },
+  ];
+
   return (
     <div className="h-[5rem] w-[25rem] bg-white bg-opacity-50 mx-auto rounded-full border-white border-opacity-60 border">
       <div className="h-full w-[22em] mx-auto flex">
         {menu.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className={
-                "flex-1 my-auto  rounded-full bg-slate-400 py-2 transition-opacity duration-500" +
-                (pathname == item.path ? " bg-opacity-30" : " bg-opacity-0")
-              }
-              onClick={() => {
-                route.push(item.path);
-              }}
-            >
-              <Image
-                src={item.icon}
-                height={32}
-                width={32}
-                alt={item.name}
-                className="cursor-pointer mx-auto"
-              ></Image>
-            </div>
-          );
+          if (item.path === "/") {
+            return (
+              <div
+                key={index}
+                className={
+                  "flex-1 my-auto rounded-full bg-slate-400 py-2 transition-opacity duration-500" +
+                  (pathname === "/" ? " bg-opacity-30" : " bg-opacity-0")
+                }
+                onClick={() => {
+                  route.push(item.route);
+                }}
+              >
+                <Image
+                  src={item.icon}
+                  height={32}
+                  width={32}
+                  alt={item.name}
+                  className="cursor-pointer mx-auto"
+                ></Image>
+              </div>
+            );
+          } else {
+            return (
+              <div
+                key={index}
+                className={
+                  "flex-1 my-auto rounded-full bg-slate-400 py-2 transition-opacity duration-500" +
+                  (pathname.startsWith(item.path)
+                    ? " bg-opacity-30"
+                    : " bg-opacity-0")
+                }
+                onClick={() => {
+                  route.push(item.route);
+                }}
+              >
+                <Image
+                  src={item.icon}
+                  height={32}
+                  width={32}
+                  alt={item.name}
+                  className="cursor-pointer mx-auto"
+                ></Image>
+              </div>
+            );
+          }
         })}
       </div>
     </div>
