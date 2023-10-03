af58aa1df8d1d6de9a06504197c3f3b9e90d75b7
banahaker <pn0818x@outlook.com>
2023-10-04 00:14:42 +0800

    feat: switch button component

---
+++ app/[userID]/(User)/SwitchButton.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const SwitchButton = ({ callback }: { callback: Function }) => {
+  const [buttonStyle, setButtonStyle] = useState("left-0");
+  const [leftStyle, setLeftStyle] = useState(
+    "py-4 w-[100px] text-center duration-500 relative text-white"
+  );
+  const [rightStyle, setRightStyle] = useState(
+    "py-4 w-[100px] text-center duration-500 relative text-md-dark-green"
+  );
+
+  return (
+    <div className="ml-[10px] flex bg-white bg-opacity-50 rounded-full mb-4 relative">
+      <div
+        className={`transition-all duration-500 w-[100px] h-[56px] absolute bg-md-dark-green rounded-full ${buttonStyle}`}
+      ></div>
+      <button
+        className={leftStyle}
+        onClick={() => {
+          setButtonStyle("left-0");
+          callback(0);
+          setLeftStyle(
+            "py-4 w-[100px] text-center duration-500 relative text-white"
+          );
+          setRightStyle(
+            "py-4 w-[100px] text-center duration-500 relative text-md-dark-green"
+          );
+        }}
+      >
+        Posts
+      </button>
+      <button
+        className={rightStyle}
+        onClick={() => {
+          setButtonStyle("left-[100px]");
+          callback(1);
+          setLeftStyle(
+            "py-4 w-[100px] text-center duration-500 relative text-md-dark-green"
+          );
+          setRightStyle(
+            "py-4 w-[100px] text-center duration-500 relative text-white"
+          );
+        }}
+      >
+        Likes
+      </button>
+    </div>
+  );
+};
+
+export default SwitchButton;
--- app/[userID]/page.tsx
+++ app/[userID]/page.tsx
@@ -5,56 +5,9 @@ import SplitBlock from "../(Layout)/splitBlock";
 import ThreadsList from "./(User)/ThreadsList";
 import Image from "next/image";
 import { useRouter, notFound } from "next/navigation";
+import SwitchButton from "./(User)/SwitchButton";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-const SwitchButton = ({ callback }: { callback: Function }) => {
-  const [buttonStyle, setButtonStyle] = useState("left-0");
-  const [leftStyle, setLeftStyle] = useState(
-    "py-4 w-[100px] text-center duration-500 relative text-white"
-  );
-  const [rightStyle, setRightStyle] = useState(
-    "py-4 w-[100px] text-center duration-500 relative text-md-dark-green"
-  );
-
-  return (
-    <div className="ml-[10px] flex bg-white bg-opacity-50 rounded-full mb-4 relative">
-      <div
-        className={`transition-all duration-500 w-[100px] h-[56px] absolute bg-md-dark-green rounded-full ${buttonStyle}`}
-      ></div>
-      <button
-        className={leftStyle}
-        onClick={() => {
-          setButtonStyle("left-0");
-          callback(0);
-          setLeftStyle(
-            "py-4 w-[100px] text-center duration-500 relative text-white"
-          );
-          setRightStyle(
-            "py-4 w-[100px] text-center duration-500 relative text-md-dark-green"
-          );
-        }}
-      >
-        Posts
-      </button>
-      <button
-        className={rightStyle}
-        onClick={() => {
-          setButtonStyle("left-[100px]");
-          callback(1);
-          setLeftStyle(
-            "py-4 w-[100px] text-center duration-500 relative text-md-dark-green"
-          );
-          setRightStyle(
-            "py-4 w-[100px] text-center duration-500 relative text-white"
-          );
-        }}
-      >
-        Likes
-      </button>
-    </div>
-  );
-};
 
 export default function UserPage({ params }: { params: { userID: string } }) {
   const UserID = decodeURIComponent(params.userID);
