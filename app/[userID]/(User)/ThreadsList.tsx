cbbe5e6ef6c385f7715b658821310bbade424c7c
HACO <jasonytonlinecomeandsee@gmail.com>
2023-10-03 15:48:06 +0800

    fix: deploy

--- app/[userID]/(User)/ThreadsList.tsx
+++ app/[userID]/(User)/ThreadsList.tsx
@@ -6,13 +6,14 @@ import { IThread } from "@/interface/Thread.interface";
 import { useRouter } from "next/navigation";
 
 const testingData: IThread = {
+  _id: "123123",
   title:
     "論文導讀 - Discovering faster matrix multiplication algorithms with reinforcement learning",
-  author: "Lazp",
-  timestamp: "123123",
+  user: "1233890",
+  createdAt: "123123",
   content:
     "這篇論文介紹了一種名為AlphaTensor的人工智慧系統，它可以發現新穎、高效且可證明正確的算法，用於矩陣乘法等基本任務。這種方法可以加速涉及矩陣乘法的普遍計算，並推進對這種基本操作的理解。AlphaTensor使用了一種名為強化學習的人工智能技術，它可以自動設計算法流程。AlphaTensor將矩陣乘法的流程參數化，然後自動尋找一套好的參數......\n這篇論文介紹了一種名為AlphaTensor的人工智慧系統，它可以發現新穎、高效且可證明正確的算法，用於矩陣乘法等基本任務。這種方法可以加速涉及矩陣乘法的普遍計算，並推進對這種基本操作的理解。AlphaTensor使用了一種名為強化學習的人工智能技術，它可以自動設計算法流程。AlphaTensor將矩陣乘法的流程參數化，然後自動尋找一套好的參數......",
-  coverImage:
+  cover:
     "https://images.unsplash.com/photo-1579407364450-481fe19dbfaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
 };
 
@@ -30,7 +31,7 @@ const Thread = ({ threadData }: { threadData: IThread }) => {
       </div>
       <div className={style.right}>
         <Image
-          src={threadData.coverImage}
+          src={threadData.cover}
           alt="coverimage"
           width={500}
           height={500}
--- interface/Thread.interface.ts
+++ interface/Thread.interface.ts
@@ -3,7 +3,6 @@ export interface IThread {
   _id: string;
   title: string;
   content: string;
-  author: string;
   cover: string;
   user: string;
 }
