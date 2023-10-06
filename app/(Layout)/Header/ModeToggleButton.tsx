d6aeaf3e86e0ade64705bdd60c3db3342f7f386a
HACO <jasonytonlinecomeandsee@gmail.com>
2023-10-06 17:58:03 +0800

    feat: change theme mode toggle bottom

--- app/(Layout)/Header/ModeToggleButton.tsx
+++ app/(Layout)/Header/ModeToggleButton.tsx
@@ -1,37 +1,21 @@
-import styles from "./ModeToggleButton.module.scss";
 import "./ModeToggleButton.scss";
 
 export default function Button() {
   return (
-    <div className={"wrapper " + styles.wrapper}>
-      <input
-        type="checkbox"
-        id={"hide-checkbox"}
-        className={styles["hide-checkbox"] + " hide-checkbox"}
-      />
-      <label htmlFor="hide-checkbox" className={"toggle " + styles.toggle}>
-        <span className={"toggle-button " + styles["toggle-button"]}></span>
-        <span
-          className={"star star-1 " + styles.star + " " + styles["star-1"]}
-        ></span>
-        <span
-          className={"star star-2 " + styles.star + " " + styles["star-2"]}
-        ></span>
-        <span
-          className={"star star-3 " + styles.star + " " + styles["star-3"]}
-        ></span>
-        <span
-          className={"star star-4 " + styles.star + " " + styles["star-4"]}
-        ></span>
-        <span
-          className={"star star-5 " + styles.star + " " + styles["star-5"]}
-        ></span>
-        <span
-          className={"star star-6 " + styles.star + " " + styles["star-6"]}
-        ></span>
-        <span
-          className={"star star-7 " + styles.star + " " + styles["star-7"]}
-        ></span>
+    <div className="toggleWrappers">
+      <input type="checkbox" className="dn hidden" id="dn" />
+      <label htmlFor="dn" className="toggle">
+        <span className="toggle__handler">
+          <span className="crater crater--1"></span>
+          <span className="crater crater--2"></span>
+          <span className="crater crater--3"></span>
+        </span>
+        <span className="star star--1"></span>
+        <span className="star star--2"></span>
+        <span className="star star--3"></span>
+        <span className="star star--4"></span>
+        <span className="star star--5"></span>
+        <span className="star star--6"></span>
       </label>
     </div>
   );
