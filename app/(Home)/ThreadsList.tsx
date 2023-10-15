f304b3ad29726c79263afa89edc8b5ed132fb665
HACO <jasonytonlinecomeandsee@gmail.com>
2023-10-15 14:31:42 +0800

    fix: home list && post title

--- app/(Home)/ThreadsList.tsx
+++ app/(Home)/ThreadsList.tsx
@@ -7,25 +7,35 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const ThreadsList = () => {
+  const [status, setStatus] = useState("loading");
   const [posts, setPosts] = useState<IThread[]>([]);
 
   useEffect(() => {
-    GetPostListAPI(setPosts);
+    GetPostListAPI(setPosts, setStatus);
   }, []);
-  if (posts.length === 0) {
+  if (status === "loading") {
+    return (
+      <div className="flex text-[50px] text-center h-full w-full select-none">
+        <p className="flex-1 h-full">Loading...</p>
+      </div>
+    );
+  } else if (posts.length === 0) {
     return (
       <div className="flex text-[50px] text-center h-full w-full select-none">
         <p className="flex-1 h-full">No post yet.</p>
       </div>
     );
+  } else {
+    return <_ThreadsList posts={posts} height="65dvh" />;
   }
-
-  return <_ThreadsList posts={posts} height="65dvh" />;
 };
 
 export default ThreadsList;
 
-async function GetPostListAPI(setPosts: Dispatch<SetStateAction<IThread[]>>) {
+async function GetPostListAPI(
+  setPosts: Dispatch<SetStateAction<IThread[]>>,
+  setStatus: Dispatch<SetStateAction<string>>
+) {
   try {
     const res = await (
       await fetch(`${API_URL}/post/list`, {
@@ -34,6 +44,7 @@ async function GetPostListAPI(setPosts: Dispatch<SetStateAction<IThread[]>>) {
     ).json();
 
     setPosts(res.postData);
+    setStatus("success");
     return;
   } catch (error) {
     console.log(error);
