3aa9779349ac66331aabcadadad3c6914789c07b
banahaker <pn0818x@outlook.com>
2023-10-15 15:32:59 +0800

    feat: login/avatar for mobile header

--- app/(Layout)/mobile/UserLogin.tsx
+++ app/(Layout)/mobile/UserLogin.tsx
@@ -51,7 +51,7 @@ const UserLogin = () => {
     const user = JSON.parse(localStorage.User || "{}");
     return (
       <div
-        className={style.userPanel + " select-none"}
+        className={style.userPanel}
         onClick={() => {
           signOut();
           localStorage.clear();
@@ -65,12 +65,6 @@ const UserLogin = () => {
             alt="Avatar"
             className="ml-1"
           ></Image>
-        </div>
-        <div className={style.user}>
-          <p className="name text-md-dark-green text-[20px] leading-6 font-bold">
-            {user.name}
-          </p>
-          <p className="text-[#006180] text-[12px] leading-3	">{user.email}</p>
         </div>
       </div>
     );
