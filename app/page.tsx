a92edcb5b7e7ee5becab74db6615bda9f17cf5fa
banahaker <pn0818x@outlook.com>
2023-10-16 13:49:33 +0800

    feat: mobile post list

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
+"use client";
+
+// Desktop Components
 import SplitBlock from "./(Layout)/splitBlock";
-import ThreadsList from "./(Home)/ThreadsList";
-import Information from "./(Home)/Information";
+import ThreadsListDesktop from "./(Home)/desktop/ThreadsList";
+import Information from "./(Home)/desktop/Information";
+// Mobile Components
+import ThreadsListMobile from "./(Home)/mobile/ThreadsList";
+import useIsMobile from "@/utils/useIsMobile";
+// Mobile Style
+import stylesMobile from "@/app/(Home)/mobile/Threads.module.scss";
 
 const Home = () => {
-  return (
-    <SplitBlock>
-      <div className="h-full w-full ">
-        <ThreadsList />
-      </div>
-      <div className="h-full">
-        <Information />
-      </div>
-    </SplitBlock>
-  );
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return <ThreadsListMobile></ThreadsListMobile>;
+  } else {
+    return (
+      <SplitBlock>
+        <div className="h-full w-full">
+          <ThreadsListDesktop />
+        </div>
+        <div className="h-full">
+          <Information />
+        </div>
+      </SplitBlock>
+    );
+  }
 };
 
 export default Home;
---
+++ components/Threads/mobile/ThreadsList.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import style from "./ThreadsList.module.scss";
+import { IThread } from "@/interface/Thread.interface";
+import { useRouter } from "next/navigation";
+
+const Thread = ({ threadData }: { threadData: IThread }) => {
+  const router = useRouter();
+  return (
+    <div
+      className={style.thread}
+      onClick={() => router.push("/post/" + threadData._id)}
+    >
+      <div className={style.preview}>
+        <h1 className={style.previewTitle}>{threadData.title}</h1>
+        <p className={style.previewContent}>{threadData.content}</p>
+      </div>
+      <div className={style.cover}>
+        <Image
+          src={threadData.cover}
+          alt="cover image"
+          style={{ objectFit: "cover" }}
+          priority
+          sizes="100%"
+          fill
+        ></Image>
+      </div>
+    </div>
+  );
+};
+
+export const ThreadsList = ({
+  posts,
+  height,
+}: {
+  posts: IThread[];
+  height?: string;
+}) => {
+  return (
+    <div className={style.threads} style={{ height: height }}>
+      {posts && posts?.length >= 1 ? (
+        posts.map((item, index) => {
+          return <Thread threadData={item} key={index} />;
+        })
+      ) : (
+        <div className={style.loading}>Loading...</div>
+      )}
+    </div>
+  );
+};
