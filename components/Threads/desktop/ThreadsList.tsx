08a7a08d5d976c8c27644e942a3e361c0f7231a5
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-17 14:52:19 +0800

    refactor: markdownToPlainText function
    Refactored the markdownToPlainText function to improve readability and maintainability. The function now uses regex to remove various elements from the markdown string, such as headings, italic, images, links, blockquotes, lists, code blocks, tables, LaTeX expressions, formulas, and line breaks. The function trims leading and trailing whitespace before returning the plain text.

--- components/Threads/desktop/ThreadsList.tsx
+++ components/Threads/desktop/ThreadsList.tsx
@@ -20,7 +20,7 @@ const Thread = ({ threadData }: { threadData: IThread }) => {
           {threadData.title}
           {threadData._id === "652cabdb45c0be8f82c54d9a" && " • 已置頂"}
         </h1>
-        <p className={style.previewContent}>{threadData.content}</p>
+        <p className={style.previewContent}>{markdownToPlainText(threadData.content)}</p>
       </div>
       <div
         className={style.cover}
@@ -61,3 +61,57 @@ export const ThreadsList = ({
     </div>
   );
 };
+
+function markdownToPlainText(markdown: string) {
+  // Remove headings
+  markdown = markdown.replace(/#+\s+(.*)/g, "$1");
+
+  // Remove bold and italic formatting
+  markdown = markdown.replace(/(\*\*|__)(.*?)\1/g, "$2");
+  markdown = markdown.replace(/(\*|_)(.*?)\1/g, "$2");
+
+  // Remove inline code formatting
+  markdown = markdown.replace(/`([^`]+)`/g, "$1");
+
+  // Remove images
+  markdown = markdown.replace(/!\[(.*?)\]\(.*?\)/g, "$1");
+
+  // Remove links
+  markdown = markdown.replace(/\[(.*?)\]\(.*?\)/g, "$1");
+
+  // Remove blockquotes
+  markdown = markdown.replace(/^\s*>.*$/gm, "");
+
+  // Remove unordered lists
+  markdown = markdown.replace(/^\s*-\s+(.*)$/gm, "$1");
+
+  // Remove ordered lists
+  markdown = markdown.replace(/^\s*\d+\.\s+(.*)$/gm, "$1");
+
+  // Remove horizontal rules
+  markdown = markdown.replace(/^\s*[-*_]{3,}\s*$/gm, "");
+
+  // Remove code blocks
+  markdown = markdown.replace(/```[\s\S]*?```/g, "");
+
+  // Remove tables
+  markdown = markdown.replace(
+    /(\|.*\|.*\|\n\|.*\|.*\|\n)(\|.*\|.*\|\n)+/g,
+    ""
+  );
+
+  // Remove inline LaTeX expressions
+  markdown = markdown.replace(/\\\((.*?)\\\)/g, "");
+
+  // Remove formulas
+  markdown = markdown.replace(/\$\$\n[\s\S]*?\n\$\$/g, "");
+  markdown = markdown.replace(/\$[\S ]*?\$/g, "");
+
+  // Remove line breaks
+  markdown = markdown.replace(/\n/g, " ");
+
+  // Trim leading and trailing whitespace
+  markdown = markdown.trim();
+
+  return markdown;
+}
