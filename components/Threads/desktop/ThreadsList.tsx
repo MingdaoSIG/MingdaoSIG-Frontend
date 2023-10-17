9da0a8282957473034b095e43720cd8ac197ef27
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-17 23:09:49 +0800

    refactor: Refactor ThreadsList component
    Refactored the ThreadsList component to improve code readability and maintainability. Made use of a separate function, MarkdownToPlainText, to convert markdown content to plain text. Replaced hard-coded values with constants for better maintainability.

--- components/Threads/desktop/ThreadsList.tsx
+++ components/Threads/desktop/ThreadsList.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 import style from "./ThreadsList.module.scss";
 import { IThread } from "@/interface/Thread.interface";
 import { useRouter } from "next/navigation";
+import MarkdownToPlainText from "@/modules/MarkdownToPlainText";
+
+const announcementSigId = "652d60b842cdf6a660c2b778";
+const pinned = [
+  "652cabdb45c0be8f82c54d9a",
+  "652e4591d04b679afdff697e"
+];
 
 const Thread = ({ threadData }: { threadData: IThread }) => {
   const router = useRouter();
@@ -10,33 +17,23 @@ const Thread = ({ threadData }: { threadData: IThread }) => {
       className={style.thread + "  cursor-pointer select-none "}
       onClick={() => router.push("/post/" + threadData._id)}
       style={{
-        backgroundColor:
-          ((threadData._id === "652cabdb45c0be8f82c54d9a" ||
-            threadData._id === "652e4591d04b679afdff697e") &&
-            "white") ||
-          "",
+        backgroundColor: pinned.includes(threadData._id) ? "white" : ""
       }}
     >
       <div className={style.preview}>
         <h1 className={style.previewTitle}>
-          {threadData.sig === "652d60b842cdf6a660c2b778" && "🔔 公告 - "}
+          {threadData.sig === announcementSigId && "🔔 公告 - "}
           {threadData.title}
-          {(threadData._id === "652cabdb45c0be8f82c54d9a" ||
-            threadData._id === "652e4591d04b679afdff697e") &&
-            " • 已置頂"}
+          {pinned.includes(threadData._id) && " • 已置頂"}
         </h1>
         <p className={style.previewContent}>
-          {markdownToPlainText(threadData.content)}
+          {MarkdownToPlainText(threadData.content)}
         </p>
       </div>
       <div
         className={style.cover}
         style={{
-          display:
-            ((threadData._id === "652cabdb45c0be8f82c54d9a" ||
-              threadData._id === "652e4591d04b679afdff697e") &&
-              "none") ||
-            "",
+          display: pinned.includes(threadData._id) ? "none" : ""
         }}
       >
         <Image
@@ -61,7 +58,7 @@ export const ThreadsList = ({
 }) => {
   return (
     <div className={style.threads} style={{ height: height }}>
-      {posts && posts?.length >= 1 ? (
+      {posts ? (
         posts.map((item, index) => {
           return <Thread threadData={item} key={index} />;
         })
@@ -71,57 +68,3 @@ export const ThreadsList = ({
     </div>
   );
 };
-
-function markdownToPlainText(markdown: string) {
-  // Remove headings
-  markdown = markdown.replace(/#+\s+(.*)/g, "$1");
-
-  // Remove bold and italic formatting
-  markdown = markdown.replace(/(\*\*|__)(.*?)\1/g, "$2");
-  markdown = markdown.replace(/(\*|_)(.*?)\1/g, "$2");
-
-  // Remove inline code formatting
-  markdown = markdown.replace(/`([^`]+)`/g, "$1");
-
-  // Remove images
-  markdown = markdown.replace(/!\[(.*?)\]\(.*?\)/g, "$1");
-
-  // Remove links
-  markdown = markdown.replace(/\[(.*?)\]\(.*?\)/g, "$1");
-
-  // Remove blockquotes
-  markdown = markdown.replace(/^\s*>.*$/gm, "");
-
-  // Remove unordered lists
-  markdown = markdown.replace(/^\s*-\s+(.*)$/gm, "$1");
-
-  // Remove ordered lists
-  markdown = markdown.replace(/^\s*\d+\.\s+(.*)$/gm, "$1");
-
-  // Remove horizontal rules
-  markdown = markdown.replace(/^\s*[-*_]{3,}\s*$/gm, "");
-
-  // Remove code blocks
-  markdown = markdown.replace(/```[\s\S]*?```/g, "");
-
-  // Remove tables
-  markdown = markdown.replace(/(\|.*\|.*\|\n\|.*\|.*\|\n)(\|.*\|.*\|\n)+/g, "");
-
-  // Remove inline LaTeX expressions
-  markdown = markdown.replace(/\\\((.*?)\\\)/g, "");
-
-  // Remove formulas
-  markdown = markdown.replace(/\$\$\n[\s\S]*?\n\$\$/g, "");
-  markdown = markdown.replace(/\$[\S ]*?\$/g, "");
-
-  // Remove horizontal rules
-  markdown = markdown.replace(/^\s*[-*_]{3,}\s*$/gm, "");
-
-  // Remove line breaks
-  markdown = markdown.replace(/\n/g, " ");
-
-  // Trim leading and trailing whitespace
-  markdown = markdown.trim();
-
-  return markdown;
-}
