fc1a51d69d18ea7f7a246ec28c368c46381eef5e
HACO <jasonytonlinecomeandsee@gmail.com>
2023-10-20 12:44:56 +0800

    feat: change editor default

--- app/new/(new)/config/markdownGuide.tsx
+++ app/new/(new)/config/markdownGuide.tsx
@@ -1,7 +1,4 @@
 export const markdownGuide = `
 # Welcome to MDSIG Post Editor
-If you're unfamiliar with Markdown, please refer to this [tutorial](https://www.markdownguide.org/).
-
-# 歡迎使用 MDSIG 文章編輯器
-如果您對於 Markdown 不熟悉，請參考這個 [教學](https://www.markdownguide.org/).
+If you're unfamiliar with Markdown, please refer to this [tutorial](https://sig.mingdao.edu.tw/post/652e4591d04b679afdff697e).
 `;
