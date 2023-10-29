4b0805ee320d50d938415b9b0b327ce99bc93774
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-29 20:08:18 +0800

    feat: Add useUserAccount hook for user authentication
    This commit adds a new hook called useUserAccount, which handles using theAuth library. It retrieves is authenticated, and performs the necessary actions based on the authentication status. It also includes a platformLogin function that sends a request to the backend log in the user using their Google access token.

---
+++ utils/useUserAccount/index.ts
@@ -0,0 +1,83 @@
+import { useState, useEffect, useCallback } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+import { User } from "@/interfaces/User";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export function useUserAccount() {
+  const { data: session, status: OAuth } = useSession();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const userLocalStorage = localStorage.getItem("User");
+
+      if (OAuth === "loading" || !userLocalStorage) setIsLoading(true);
+
+      if (OAuth === "authenticated") {
+        try {
+          const accessToken = (session as any)?.accessToken;
+          const { token, data } = await platformLogin(accessToken);
+          localStorage.setItem("token", token);
+          localStorage.setItem("User", JSON.stringify(data));
+          setUserData(data);
+          setIsLogin(true);
+          setIsLoading(false);
+        } catch (error) {
+          console.error(error);
+          setIsLogin(false);
+          signOut();
+        }
+      } else if (OAuth !== "loading") {
+        setIsLogin(false);
+        setIsLoading(false);
+      }
+    })();
+  }, [OAuth, session]);
+
+  useEffect(() => {
+    if (!isLogin) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("User");
+    }
+  }, [isLogin]);
+
+  const login = useCallback(() => {
+    signIn();
+  }, []);
+
+  const logout = useCallback(() => {
+    signOut();
+  }, []);
+
+  return { isLogin, userData, isLoading, login, logout };
+}
+
+async function platformLogin(accessToken: string) {
+  try {
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("googleToken", accessToken);
+    const response = await (
+      await fetch(`${API_URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: urlencoded,
+      })
+    ).json();
+
+    if (response.status !== 2000) throw new Error("Failed to login to platform");
+
+    return {
+      token: response.authorization.toString().split(" ")[1],
+      data: response.data,
+    };
+  } catch (error) {
+    throw new Error("Failed to login to platform");
+  }
+}
