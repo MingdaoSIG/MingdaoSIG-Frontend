5a3f3e7db861a58e12ca164eecd67e361a135f7a
OnCloud125252 <anonymousaaaa41414141@gmail.com>
2023-10-30 15:17:25 +0800

    feat: Add NotFound component to hashtag page
    - Added the NotFound component to the hashtag page
    - Replaced the previous code with the NotFound component
    - Set the code to "1969" and the message to "Page Under Construction"

--- app/hashtag/page.tsx
+++ app/hashtag/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { NotFound } from "@/components/NotFound";
 
 export default function HashtagAll() {
-  const route = useRouter();
   return (
-    <div className="flex flex-col m-auto">
-      <Image
-        src={"/images/404.svg"}
-        width={189}
-        height={200}
-        alt={"404"}
-        className="m-auto"
-      />
-      <h1 className="text-2xl mx-auto leading-2xl">1969</h1>
-      <p className="text-lg mx-auto">Page Under Construction.</p>
-      <button
-        className="bg-[#0090BD] bg-opacity-60 rounded-2xl w-48 h-14 block m-auto text-white mt-10 text-[20px]"
-        onClick={() => {
-          route.push("/");
-        }}
-      >
-        Back to home
-      </button>
-    </div>
+    <NotFound
+      code="1969"
+      message="Page Under Construction"
+    />
   );
 }
