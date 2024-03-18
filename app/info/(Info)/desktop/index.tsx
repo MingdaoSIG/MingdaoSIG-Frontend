8c209203d63bddb7a47dcacbe0658dd78a03e9b8
Haco <haco.tw@gmail.com>
2024-03-18 09:31:42 +0800

    feat: post page

--- app/info/(Info)/desktop/index.tsx
+++ app/info/(Info)/desktop/index.tsx
@@ -1,7 +1,20 @@
+// Styles
+import styles from "./index.module.scss";
+
 export default function Desktop() {
   return (
-    <div>
-      Desktop
+    <div className={styles.mobileView}>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          <h1>MDSIG 平台資訊</h1>
+        </div>
+        <div className={styles.content}>
+          {/* <MdPreview
+            modelValue="rule"
+            previewTheme="github"
+          /> */}
+        </div>
+      </div>
     </div>
   );
 }
--- app/info/(Info)/mobile/index.tsx
+++ app/info/(Info)/mobile/index.tsx
@@ -1,7 +1,23 @@
+// Styles
+import styles from "./index.module.scss";
+
+// Modules
+import { MdPreview } from "md-editor-rt";
+
 export default function Mobile() {
   return (
-    <div>
-      Mobile
+    <div className={styles.mobileView}>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          <h1>MDSIG 平台資訊</h1>
+        </div>
+        <div className={styles.content}>
+          {/* <MdPreview
+            modelValue="rule"
+            previewTheme="github"
+          /> */}
+        </div>
+      </div>
     </div>
   );
 }
