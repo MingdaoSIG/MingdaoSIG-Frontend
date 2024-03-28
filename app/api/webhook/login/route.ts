d88d87ab4714a5c24e39a4adc62d9f3cae1965c3
HACO <haco.tw@gmail.com>
2024-03-29 00:29:21 +0800

    feat: login log

--- app/api/webhook/login/route.ts
+++ app/api/webhook/login/route.ts
@@ -1,35 +1,78 @@
 import axios from "axios";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req : NextRequest) {
-  const url = new URL(req.url);
-  const name = url.searchParams.get("name");
-  const description = url.searchParams.get("description");
-
-  if (!name) {
-    return Response.json({"message": "Missing name parameter!"});
-  }
-
-  if (!description) {
-    return Response.json({"message": "Missing description parameter!"});
-  }
-
+export async function POST(req : NextRequest) {
+  const data = await req.formData();
   const content = {
     username: "MDSIG Login",
-    avatar_url: "https://sig.mingdao.edu.tw/images/logo_mobile.png",
+    avatar_url: "https://cdn.discordapp.com/attachments/1222932958667870279/1222933028259496080/sig2_pfp__1.png",
     embeds: [
       {
-        "title": name,
+        "title": data.get("name"),
+        "description": data.get("description"),
         "color": parseInt("0x34e718"),
+        "thumbnail": {
+          "url": data.get("avatar"),
+        },
         "fields": [
           {
-            "name": "Description",
-            "value": description,
+            "name": "ID",
+            "value": data.get("id"),
+            "inline": true
+          },
+          {
+            "name": "Custom ID",
+            "value": data.get("customId"),
+            "inline": true
+          },
+          {
+            "name": "Identity",
+            "value": data.get("identity"),
+            "inline": true
+          },
+          {
+            "name": "Email",
+            "value": data.get("email"),
+            "inline": true
+          },
+          {
+            "name": "Badge",
+            "value": data.get("badge") || "No Data",
+            "inline": true
+          },
+          {
+            "name": "\u200b",
+            "value": "\u200b",
+            "inline": true
+          },
+          {
+            "name": "Code",
+            "value": data.get("code"),
+            "inline": true
+          },
+          {
+            "name": "Class",
+            "value": data.get("class"),
+            "inline": true
+          },
+          {
+            "name": "\u200b",
+            "value": "\u200b",
+            "inline": true
+          },
+          {
+            "name": "Login IP",
+            "value": req.headers.get("x-real-ip") || req.headers.get("x-forwarded-for") || req.ip,
             "inline": false
           },
-        ]
-      }
-    ]
+        ],
+        "timestamp": new Date().toISOString(),
+        "footer": {
+          "text": "MDSIG 2.0 Login System",
+          "icon_url": "https://cdn.discordapp.com/attachments/1222932958667870279/1222933028259496080/sig2_pfp__1.png?ex=66180422&is=66058f22&hm=a0229ab4733f6a2c4d2db217500663987351acdee83ff724afcfddc84e61fc45&",
+        },
+      },
+    ],
   };
 
   axios(String(process.env.NEXT_PUBLIC_WEBHOOK_LOGIN),{
@@ -39,7 +82,8 @@ export async function GET(req : NextRequest) {
     },
     data: JSON.stringify(content),
   });
-  return Response.json({"message": "Request Sent!"});
+  
+  return NextResponse.json({ "message": "Request Sent!" });
 }
 
 export const dynamic = "force-dynamic";
