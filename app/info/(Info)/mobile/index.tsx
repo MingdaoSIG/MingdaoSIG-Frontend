4aa9408ce4fe00b635dec2010fb3624b7ec9018e
HACO <haco.tw@gmail.com>
2024-05-10 17:29:25 +0800

    feat: info

--- app/info/(Info)/mobile/index.tsx
+++ app/info/(Info)/mobile/index.tsx
@@ -2,7 +2,7 @@
 import styles from "./index.module.scss";
 
 // Modules
-import { MdPreview } from "md-editor-rt";
+import Link from "next/link";
 
 export default function Mobile() {
   return (
@@ -12,10 +12,29 @@ export default function Mobile() {
           <h1>MDSIG 平台資訊</h1>
         </div>
         <div className={styles.content}>
-          {/* <MdPreview
-            modelValue="rule"
-            previewTheme="github"
-          /> */}
+          <p>
+            <h1 className={styles.links}>常用連結</h1>
+            <Link href="/post/652cabdb45c0be8f82c54d9a">Rule</Link>
+            <br />
+            <Link href="/post/65325fce0b891d1f6b5b3131">About</Link>
+            <br />
+            <Link href="/post/652cabdb45c0be8f82c54d9a">Markdown Tourist</Link>
+
+            <h1 className={styles.links2}>聯絡我們</h1>
+            <Link href="mailto:mdsig20@ms.mingdao.edu.tw" target="_blank">Email</Link>
+            <br />
+            <Link href="https://www.instagram.com/mdsig.dev" target="_blank">Instagram</Link>
+
+            <h1 className={styles.links2}>回報錯誤</h1>
+            <Link href="https://forms.gle/ADMk67dcTURxjn979" target="_blank">Join Us</Link>
+            <br />
+            <Link href="https://forms.gle/QJbcdvTvfcFkNJ1PA" target="_blank">MDSIG 2.0 Report</Link>
+
+            <h1 className={styles.links2}>小小彩蛋</h1>
+            <Link href="/images/banner.png" target="_blank">Banner</Link>
+            <br />
+            <Link href="/videos/sig.mp4" target="_blank">Dynamic Banner</Link>
+          </p>
         </div>
       </div>
     </div>
