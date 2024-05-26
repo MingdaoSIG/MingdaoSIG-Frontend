ba34bfd2c89b43071834a69c381c9b92f1871f9d
HACO <haco.tw@gmail.com>
2024-05-27 01:28:10 +0800

    feat: chart (not yet)

--- app/dashboard/(Dashboard)/Desktop/index.tsx
+++ app/dashboard/(Dashboard)/Desktop/index.tsx
@@ -5,6 +5,40 @@ import styles from "./index.module.scss";
 
 // Modules
 import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import "chart.js/auto";
+
+
+const Bar = dynamic(() => import("react-chartjs-2").then((mod) => mod.Bar), {
+  ssr: false,
+});
+
+const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "各 SIG 發文數量",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 export default function Desktop() {
   const [userCount, setUserCount] = useState(0);
@@ -47,7 +81,7 @@ export default function Desktop() {
         });
     }
 
-  }, [date, likeCount, postUserCount, userCount, validPostCount]);
+  }, []);
 
 
   return (
@@ -88,6 +122,7 @@ export default function Desktop() {
           </div>
         </div>
         <div className={styles.sigChart}>
+          <Bar data={data} />
         </div>
       </div>
     </div >
