0c626ebe6910ec52953326be818a215a23911b70
OnCloud <anonymousaaaa41414141@gmail.com>
2024-08-07 13:15:52 +0800

    feat: :sparkles: fix chuk loading
    Keep the same artifacts across all builds on different nodes to suit with load balancing.

--- next.config.js
+++ next.config.js
@@ -15,6 +15,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      config.output.filename = "static/chunks/[name].js";
+      config.output.chunkFilename = "static/chunks/[name].js";
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
